dbaf1e168fd0211bee64c67071dd972209c51ab9
Matthias Flosbach <flosbach.m@web.de>
2021-08-13 23:20:27 +0200

    first tests work, need to fix todos

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pylatex.utils import NoEscape
 import argparse
 import formel_tree as ft
 
+#TODO: include math to use implies, remove margin from sides
 
 def models(vars):
     """
@@ -24,25 +25,29 @@ def main(in_file, doc):
 
     try:
         with open(in_file, "r") as file:
-            formular = file.read()
+            formel = file.read()
     except Exception as e:
         print(f"Error opening/reading the input file: {e}")
         exit()
 
-    for var in formular.split(" "):
+    for var in formel.split(" "):
         if var not in logic_operators:
             var = var.replace("-", "")
             variables.update({var: 0})
-
-    init_table(doc, len(variables))
-    #with \\land /\ and \\lor \/
+    
+    t = ft.tree(formel)
+    evaluated = t.evaluate()
+    init_table(doc, len(variables) + len(evaluated))
+    #write header of table
+    table_write_line(doc, list(variables.keys()) + list(evaluated.keys()))
 
     for model in models(variables):
-        #models should return an dic with the updated values for the variables
-        #so just updated values in the tree
-        #evaluate
-        #write line to doc
-        pass
+        last_line = True if sum(model) == len(variables) else False 
+        t.update_values(variables)
+        evaluated = t.evaluate() 
+        table_write_line(doc, model + list(evaluated.values()), last_line)
+    
+    return
 
 def init_table(doc, size):
     center = "|".join(["c"]*size)
@@ -57,6 +62,14 @@ def table_write_line(doc, arr, last_line=False):
     else:
         #if we write atoms and subformules
         #just put a & between every element of arr
+        for index, ele in enumerate(arr):
+            ele = ele.replace("-", " \\neg ")
+            ele = ele.replace(" /\\ ", " \\land ")
+            ele = ele.replace(" \\/ ", " \\lor ")
+            #implies requires to use a math package and i dont know how to include it
+            ele = ele.replace(" => ", " \\Rightarrow ")
+            ele = ele.replace(" <=> ", " \\iff ")
+            arr[index] = "$" + ele + "$"
         arg_str = " & ".join(arr)
     line = arg_str + r"\\"
 
@@ -71,7 +84,6 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("input", help="The file with the logic you want the truth table for", type=str)
     p.add_argument("output", help="The filename were you want to save the pdf with the truth table", type=str)
-    #p.add_argument("-v", "--verbose", help="If a variable occurs negated in the formular, print the negated and non negated value to the table. Default is to not print the negated value", type=bool, default=False)
     args = p.parse_args()
 
     doc = Document(args.output.replace(".pdf", ""))
